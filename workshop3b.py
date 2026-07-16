9938f91ba81839cb6e9f793502f7f126570238d0
Nothing-888 <304258016+Nothing-888@users.noreply.github.com>
2026-07-16 16:14:53 +0700

    fix a little bug

--- workshop3b.py
+++ workshop3b.py
@@ -1,11 +1,12 @@
 print("เป้าหมาย 50,000")
 
 total = 0
-i = 1
+i = 0
 while total < 50000 :
+    i += 1
     shop = int(input(f"ร้านที่ {i} เก็บ : "))
     total += shop
-    i += 1
+    
     
 
 print("วันนี้เก็บได้ทั้งหมด : ",total)
