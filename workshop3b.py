30caf4d23b1b3de620e02b830e11e76ce2c9cb01
Nothing-888 <304258016+Nothing-888@users.noreply.github.com>
2026-07-16 16:13:09 +0700

    New Update

--- workshop3b.py
+++ workshop3b.py
@@ -1,9 +1,12 @@
-num = int(input("ไปเก็บกี่ร้าน : "))
-total = 0
+print("เป้าหมาย 50,000")
 
-for i in range(1,num) :
+total = 0
+i = 1
+while total < 50000 :
     shop = int(input(f"ร้านที่ {i} เก็บ : "))
     total += shop
+    i += 1
+    
 
 print("วันนี้เก็บได้ทั้งหมด : ",total)
 print(f"เก็บได้ {i} ร้าน")
